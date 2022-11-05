7aaefc39ece8f4dcf600b81d0807b77304d6198f
Itamar Axelrad <iaxelrad@gmail.com>
2022-11-05 12:04:55 +0200

    Adding reducer to App.tsx File

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import { Provider } from 'react-redux';
 import { ThemeManager } from './components/ThemeManager';
-const App = () => {
-  return (
+import { store } from './state';
+const App = () => (
+  <Provider store={store}>
     <ThemeManager>
       <SafeAreaView>
         <Text>Hello world</Text>
       </SafeAreaView>
     </ThemeManager>
-  );
-};
+  </Provider>
+);
 export default App;
